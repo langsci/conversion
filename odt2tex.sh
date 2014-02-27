f01d2ee4b53c25c1dc5ee44aa2e62dcca0c62c95
Schmalhans <lqfb@glottotopia.de>
2014-02-27 16:52:24 +0100

    intial version of odt2tex converter

---
+++ odt2tex.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+w2l -clean -wrap_lines_after=0 -multilingual=false -float_table=true -float_figure=true -use_caption=true -image_options="width=\textwidth" -use_tipa=true -use_bibtex=true -ignore_double_spaces=true -multilingual=false -formatting=ignore_most -use_color=false $1
