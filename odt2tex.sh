2978b0062d9d308e5660ff1e04f5d8b0dc0898e7
Schmalhans <lqfb@glottotopia.de>
2014-02-27 16:53:16 +0100

    intial version of odt2tex converter

--- odt2tex.sh
+++ odt2tex.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# This converter converts an odt document to a tex document, 
+# The output can then be pdftexed.
+# The script does not work on doc(x), so you have to first use OpenOffice or MS-Word and save the file as odt
+
 w2l -clean -wrap_lines_after=0 -multilingual=false -float_table=true -float_figure=true -use_caption=true -image_options="width=\textwidth" -use_tipa=true -use_bibtex=true -ignore_double_spaces=true -multilingual=false -formatting=ignore_most -use_color=false $1
