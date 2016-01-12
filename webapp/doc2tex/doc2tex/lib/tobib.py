e059d25f3af80077025396e56291227906122006
Sebastian Nordhoff <sebastian.nordhoff@langsci-press.org>
2016-01-12 16:19:49 +0100

    started bib parsing

--- webapp/doc2tex/doc2tex/lib/tobib.py
+++ webapp/doc2tex/doc2tex/lib/tobib.py
@@ -1,52 +1,56 @@
-bibliography converter
-article
-incollection
-book 
+#bibliography converter
+#article
+#incollection
+#book 
+import sys
 import re
 
 YEAR = '[12][78901][0-9][0-9][a-f]'
-EDITOR = "(eds?\.?)"
+EDITOR = re.compile("(eds?\.?)")
 IN = 'In'
 ENQUOTED = """["'`].*["']"""
-PAGES = "[Pp]*\.? *[0-9]-+[0-9]"
-PUBADDR = "(.+) *: *(.+)"
+PAGES = re.compile("[Pp]*\.? *[0-9][-–]+[0-9]")
+PUBADDR = re.compile("(.+) *: *(.+)")
 VOLNUM = "([0-9]+) *(\(?[0-9]\)?)"
 
 class Record():
-  def __init__(s):    
-    typ = misc
-    if eds:
-      typ = incollection
-    elif pages:
-      typ = journal
-    elif pubaddr:
-      typ = book
-
-  
-  typ = None
-  key = None
-  title = None
-  booktitle = None
-  author = None
-  editor = None
-  year = None
-  journal = None
-  volume = None
-  number  = None
-  pages = None
-  address = None
-  publisher = None
-  note = None
+  def __init__(self,s):    
+    self.typ = "misc"
+    if  EDITOR.search(s):
+      self.typ = "incollection"
+    elif  PAGES.search(s):
+      self.typ = "article"
+    elif PUBADDR.search(s):
+      self.typ = "book" 
+    self.key = None
+    self.title = None
+    self.booktitle = None
+    self.author = None
+    self.editor = None
+    self.year = None
+    self.journal = None
+    self.volume = None
+    self.number  = None
+    self.pages = None
+    self.address = None
+    self.publisher = None
+    self.note = None
   
   def tobibtex(self):
-    pass
+    print(self.typ)
 
 def getRecords(fn, splitter="\n\n"):
   c = open(fn).read()
   return [Record(s) for s in c.split(splitter)]
 
-records = getRecords(f)
+#records = getRecords(f)
 
-for record in records:
-  record.tobibtex()
+#for record in records:
+  #record.tobibtex()
   
+if __name__=="__main__":
+  fn = sys.argv[1]
+  lines = open(fn).readlines()
+  for l in lines:
+    r = Record(l)
+    r.tobibtex()
