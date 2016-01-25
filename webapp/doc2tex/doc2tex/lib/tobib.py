aab0171c61d0c1eaa7d72fc7fdebf335647f1a12
Sebastian Nordhoff <sebastian.nordhoff@langsci-press.org>
2016-01-25 15:17:14 +0100

    bibtex output working

--- webapp/doc2tex/doc2tex/lib/tobib.py
+++ webapp/doc2tex/doc2tex/lib/tobib.py
@@ -14,28 +14,32 @@ ENQUOTED = """["'`].*["']"""
 PAGES = re.compile("[Pp]*\.? *[0-9][-–]+[0-9]")
 PUBADDR = re.compile("(.+) *: *(.+)")
 VOLNUM = "([0-9]+) *(\(?[0-9]\)?)"
-BOOK = re.compile("(?P<author>.*?)[., ]*\(?(?P<year>[12][78901][0-9][0-9][a-f]?)\)?[., ]*(?P<title>.*)\. +(?P<address>.+) *: *(?P<publisher>.+)")
+BOOK = re.compile("(?P<author>.*?)[., ]*\(?(?P<year>[12][78901][0-9][0-9][a-f]?)\)?[., ]*(?P<title>.*)\. +(?P<address>.+) *: *(?P<publisher>.*?)\.")
 ARTICLE = re.compile("(?P<author>.*?)[., ]*\(?(?P<year>[12][78901][0-9][0-9][a-f]?)\)?[., ]*(?P<title>.*)\. +(?P<journal>.*) (?P<number>[-0-9]+)\. (?P<pages>[-–0-9]+)")
-INCOLLECTION = re.compile("(?P<author>.*?)[., ]*\(?(?P<year>[12][78901][0-9][0-9][a-f]?)\)?[., ]*(?P<title>.*)\. In (?P<editor>.*) \(eds?\.\), (?P<booktitle>.*), (?P<pages>[-–0-9]+). (?P<address>.+) *: *(?P<publisher>.+)")
+INCOLLECTION = re.compile("(?P<author>.*?)[., ]*\(?(?P<year>[12][78901][0-9][0-9][a-f]?)\)?[., ]*(?P<title>.*)\. In (?P<editor>.*) \(eds?\.\), (?P<booktitle>.*), (?P<pages>[-–0-9]+). (?P<address>.+) *: *(?P<publisher>.+)\.")
 
+FIELDS = ["key",
+    "title",
+    "booktitle",
+    "author",
+    "editor",
+    "year",
+    "journal",
+    "volume",
+    "number",
+    "pages",
+    "address",
+    "publisher",
+    ]
+    
 class Record():
   def __init__(self,s):    
-    self.typ = "misc"
-    if  EDITOR.search(s):
-      self.typ = "incollection"
-      self.parseincollection(s)
-    elif  PAGES.search(s):      
-      self.typ = "article"
-      self.parsearticle(s)
-    elif PUBADDR.search(s):
-      self.typ = "book" 
-      self.parsebook(s)
-    else:
-      self.parsemisc(s)
+    self.orig = s
+    self.typ = "misc"    
     self.key = None
     self.title = None
     self.booktitle = None
-    self.author = None
+    self.author = "Anonymous"
     self.editor = None
     self.year = None
     self.journal = None
@@ -44,49 +48,69 @@ class Record():
     self.pages = None
     self.address = None
     self.publisher = None
-    self.note = None
+    self.note = None 
+    if  EDITOR.search(s):
+      self.typ = "incollection"
+      m = INCOLLECTION.search(s)
+      if m:
+        self.author = m.group('author')
+        self.editor = m.group('editor')
+        self.title = m.group('title')
+        self.booktitle = m.group('booktitle')
+        self.year = m.group('year') 
+        self.address = m.group('address')
+        self.publisher = m.group('publisher')
+        self.pages = m.group('pages') 
+    elif  PAGES.search(s):      
+      self.typ = "article"
+      m = ARTICLE.search(s)
+      if m:
+        self.author = m.group('author')
+        self.title = m.group('title')
+        self.year = m.group('year')
+        self.journal = m.group('journal')
+        self.number = m.group('number')
+        self.pages = m.group('pages')   
+    elif PUBADDR.search(s):
+      self.typ = "book"  
+      m = BOOK.search(s)
+      if m:
+        self.author = m.group('author')
+        self.title = m.group('title')
+        self.year = m.group('year')
+        self.address = m.group('address')
+        self.publisher = m.group('publisher')
+    else:
+      self.parsemisc(s) 
+    #self.bibstring = self.tobibtex()   
+    try:
+      self.author = self.author.replace('&', ' and ')
+    except AttributeError:
+      pass 
+    try:
+      self.editor = self.editor.replace('&', ' and ')
+    except AttributeError:
+      pass
+    authorpart = "Anonymous"
+    yearpart = "9999" 
+    try: 
+      authorpart = self.author.split(',')[0].split(' ')[0] 
+    except AttributeError: 
+      authorpart = self.editor.split(',')[0].split(' ')[0] 
+    try:
+      yearpart = self.year[:4]
+    except TypeError:
+      pass
+    key = authorpart+yearpart 
+    bibstring="@%s{%s,\n\t"%(self.typ,key)    
+    fields = [(f,self.__dict__[f]) for f in self.__dict__ if f in FIELDS and self.__dict__[f]!=None]
+    bibstring+=",\n\t".join(["%s = {%s}"%f for f in fields])
+    bibstring+="\n}"  
+    #print bibstring
+    self.bibstring = bibstring
     
-  def parsearticle(self,s):  
-    m = ARTICLE.search(s)
-    if m:
-      self.author = m.group('author')
-      self.title = m.group('title')
-      self.year = m.group('year')
-      self.journal = m.group('journal')
-      self.number = m.group('number')
-      self.pages = m.group('pages')      
-      pprint.pprint(self.__dict__)
-      
-  def parseincollection(self,s):  
-    m = INCOLLECTION.search(s)
-    if m:
-      self.author = m.group('author')
-      self.editor = m.group('editor')
-      self.title = m.group('title')
-      self.title = m.group('booktitle')
-      self.year = m.group('year')
-      self.address = m.group('address')
-      self.publisher = m.group('publisher')
-      self.pages = m.group('pages')
-      pprint.pprint(self.__dict__)
-  
-  def parsemisc(self,s):  
-    pass
-  
-  def parsebook(self,s):  
-    m = BOOK.search(s)
-    if m:
-      self.author = m.group('author')
-      self.title = m.group('title')
-      self.year = m.group('year')
-      self.address = m.group('address')
-      self.publisher = m.group('publisher')
-      pprint.pprint(self.__dict__)        
-    
-  def tobibtex(self):
-    pass
-    #print(self.typ)
-    #pprint.pprint(self.__dict__)
+     
+
 
 def getRecords(fn, splitter="\n\n"):
   c = open(fn).read()
@@ -103,5 +127,7 @@ if __name__=="__main__":
   for l in lines:
     if l.strip=='':
       continue
-    r = Record(l)
-    r.tobibtex()
+    r = Record(l) 
+    print r.bibstring
+    #r.tobibtex()
+  
