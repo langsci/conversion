9537955263adc03f805ac76466eb34c6247f9e9c
Schmalhans <lqfb@glottotopia.de>
2017-05-29 22:05:40 +0200

    init

---
+++ xlingpaper/xlingpaper2langsci.py
@@ -0,0 +1,64 @@
+import sys
+import xml.etree.ElementTree as ET
+
+class lingPaper():
+  def __init__(self,el):
+    self.chapters = [chapter(c) for c in el.findall('chapter')]
+    self.frontmatter = el.find('frontmatter')
+    self.backmatter = el.findall('backmatter')  
+    
+  def __str__(self):   
+    return '\n'.join([str(ch) for ch in self.chapters])
+
+class genericsection():
+  def __init__(self,el):
+    self.el = el
+    self.ID = el.attrib["id"]
+    self.title = el.find("secTitle").text 
+    if self.title == None:
+      self.title = ''
+    self.preamble = self.getPreamble()
+    self.subsections = self.getSubsections()
+    #print(self.subsections)
+    
+  def getPreamble(self):
+    pass
+    
+  def getSubsections(self):
+    pass
+  
+  def __str__(self):
+    titlestring = self.title
+    preamblestring = ''
+    subsectionstring =  '\n'.join([str(ch) for ch in self.subsections])
+    return '\n'.join([titlestring,preamblestring,subsectionstring])
+  
+  
+
+class chapter(genericsection):
+  def getSubsections(self):
+    return [section1(s) for s in self.el.findall('section1')]
+  
+  
+class section1(genericsection):
+  def getSubsections(self):
+    return [section2(s) for s in self.el.findall('section2')]
+  
+  
+  
+class section2(genericsection):
+  
+  def getSubsections(self):
+    return self.el.findall('section3')
+  
+    
+  
+    
+if __name__ == "__main__":
+  fn = sys.argv[1]
+  print(fn)
+  tree = ET.parse(fn)
+  root = tree.getroot()
+  doc = lingPaper(root)
+  print(doc)
+
