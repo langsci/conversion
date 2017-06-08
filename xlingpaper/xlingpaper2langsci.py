e9cb18e501c82ae4153a80650474809de7a4b78c
Schmalhans <lqfb@glottotopia.de>
2017-06-08 18:11:52 +0200

    convert all tags

--- xlingpaper/xlingpaper2langsci.py
+++ xlingpaper/xlingpaper2langsci.py
@@ -25,48 +25,136 @@ class textelement():
     text = ''
     if te.tail:
       tail = te.tail
+      if tail[-1].strip() == '': #replace all trailing white space by ' '
+        tail = "%s "%tail.strip()
     if te.text:
       text = te.text
     if te.tag == 'object':
       typ = te.attrib["type"]
       if typ == 'tItalic':
         return '\\textit{%s}%s'%(text,tail)
-      raise ValueError
     if te.tag == 'langData':
       lang = te.attrib["lang"]
       if lang == 'lVernacular':
         return '\\vernacular{%s}%s'%(text,tail) 
-      raise ValueError
     if te.tag == 'gloss':
       lang = te.attrib["lang"]
       if lang == 'lGloss':
         return '\\gloss{%s}%s'%(text,tail) 
-      raise ValueError
     if te.tag == 'caption':
-        return '\\caption{%s}%s'%(text,tail)  
+        return '\n%%\\caption{%s}%s\n'%(text,tail)  
     if te.tag == 'free':
-        return '\\glt %s\n %s'%(text,tail)  
+        return '\\glt %s %s'%(text,tail)  
     if te.tag == 'td':
-        return '%s & %s'%(text,tail)  
+        return '%s %s & %s'%(text,''.join([self.treatTextElement(x) for x in te]) ,tail)         
     if te.tag == 'th':
-        return '%s & %s'%(text,tail)  
+        return '%s %s & %s'%(text,''.join([self.treatTextElement(x) for x in te]) ,tail)  
+    if te.tag == 'chart':
+        return ''.join([self.treatTextElement(x) for x in te]) 
+    if te.tag == 'interlinear':
+        return ''.join([self.treatTextElement(x) for x in te])   
+    if te.tag == 'img':
+        return '\\includegraphics[width=\\textwidth]{ %s}'%te.attrib['src']
+    if te.tag == 'lineGroup':
+        numberoflines = len(te)
+        lls = numberoflines * 'l' #count how many src/imt lines there are
+        return '\\g%s %s '% (lls,  ''.join([self.treatTextElement(x) for x in te]) )
+    if te.tag == 'line':   
+        try:
+          linebody = te.find('langData').text #check for embedded elements
+        except AttributeError:
+          linebody = te.find('gloss').text #check for embedded elements          
+        return '1%s2\\\\\n'% linebody
+    if te.tag == 'table':        
+        return self.treattabular(te)
+    if te.tag == 'figure':  
+      label = te.attrib.get('id', False) 
+      labelstring = ''
+      if label:
+        labelstring = '\\label{fig:%s} '%label 
+      figurebody = ' '.join([self.treatTextElement(x) for x in te])
+      return '\n\n\\begin{ figure}\n%s%s\n\\end{ figure}\n\n' % (figurebody,labelstring)   
+    
+    if te.tag == 'tablenumbered':  
+      label = te.attrib.get('id', False) 
+      labelstring = ''
+      if label:
+        labelstring = '\\label{tab:%s} '%label 
+      tablebody = ' '.join([self.treatTextElement(x) for x in te])
+      return '\n\n\\begin{ table}\n%s%s\n\\end{ table}\n\n' % (tablebody,labelstring)   
+    if te.tag == 'tr':   
+        trbody = ' '.join([self.treatTextElement(x) for x in te])
+        return '%s\\\\\n' % (trbody)   
+    if te.tag == 'p':  
+        text = ' '
+        try:
+          text=te.text.strip()
+        except AttributeError:
+          pass 
+        return "%s %s\n\n" %(text, ''.join([self.treatTextElement(x) for x in te]))
+    if te.tag == 'exampleRef': 
+      label  = te.attrib.get('num')
+      return '\\REF{ ex:%s} %s'% (label, tail)   
+    if te.tag == 'figureRef': 
+      label  = te.attrib.get('figure')
+      return '\\figref{ ex:%s} %s'% (label, tail)
+    if te.tag == 'sectionRef': 
+      label  = te.attrib.get('sec')
+    if te.tag == 'tablenumberedRef': 
+      label  = te.attrib.get('table')
+      return '\\tabref{ ex:%s} %s'% (label, tail)
+    if te.tag == 'example': 
+      label = te.attrib.get('num', False) 
+      labelstring = ''
+      if label:
+        labelstring = '\\label{ex:%s} '%label 
+      exbody = ''.join([self.treatTextElement(x) for x in te])
+      return '\n\\ea%s\n%s\n\\z\n\n' % (labelstring, exbody)   
+    if te.tag == 'endnote':        
+        label = te.attrib.get('id', False)
+        labelstring = ''
+        if label:
+          labelstring = '\\label{fn:%s} '%label
+        fnbody = ' '.join([self.treatTextElement(x) for x in te])
+        return '\\footnote{%s%s\n}%%\n' % (labelstring, fnbody)
     if te.text == None:
       return ''  
     print(te,te.text)
     raise ValueError
     return te.text
   
+  def treattabular(self,el):
+    numberofcolumns = len(el.find('tr'))
+    columntypes = numberofcolumns * 'l'
+    caption = el.find('caption').text
+    trs = el.findall('tr')
+    rows = ''.join([self.treatTextElement(tr) for tr in trs])      
+    return """\n\\begin{ tabular}{%s}  
+  %s\\end{ tabular}
+\\caption{%s}
+    """%(columntypes,rows,caption)
+  
 class paragraph(textelement):
-  pass
+  def getText(self,el):  
+    return self.treatTextElement(el)
+  
   
 class figure(textelement):
-  pass
+  def getText(self,el):  
+    return self.treatTextElement(el)
+
   
-class example(textelement):
-  pass
+class example(textelement):   
+  def getText(self,el):  
+    return self.treatTextElement(el)
+
+class chart(textelement):
+  def getText(self,el):  
+    return self.treatTextElement(el)
   
 class tablenumbered(textelement):
-  pass    
+  def getText(self,el):  
+    return self.treatTextElement(el)
 
 class genericsection():
   def __init__(self,el):
@@ -91,6 +179,7 @@ class genericsection():
         children4preamble.append(ch)
       else:
         break
+    #print(self.tag,len(children4preamble))
     return [self.treatelement(el) for el in children4preamble]
       
     
@@ -103,6 +192,8 @@ class genericsection():
       return example(el)
     if el.tag=='tablenumbered':
       return tablenumbered(el)
+    if el.tag=='chart':
+      return chart(el)
     raise ValueError
     
     
